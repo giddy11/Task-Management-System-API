fc7587201843a724027e08841601970fc7349ebf
giddy11 <edoghotugiddy@gmail.com>
2025-07-16 10:50:13 +0100

    Add DbContext with DbSet configurations for core and user entities

---
+++ TaskManagement.Persistence/TaskManagementDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagement.Domain;
+using TaskManagement.Domain.Projects;
+using TaskManagement.Domain.TodoTasks;
+using TaskManagement.Domain.UserManagement;
+
+namespace TaskManagement.Persistence;
+
+public class TaskManagementDbContext : DbContext
+{
+    public TaskManagementDbContext(DbContextOptions<TaskManagementDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TaskManagementDbContext).Assembly);
+    }
+
+    // Core Domain Entities
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<TodoTask> Tasks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Label> Labels { get; set; }
+
+
+    //user management entities
+    public DbSet<User> Users { get; set; }
+}
