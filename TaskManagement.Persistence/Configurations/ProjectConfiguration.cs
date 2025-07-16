e364d7041ff0c988441c6f782ae5e8bb343cd7f7
giddy11 <edoghotugiddy@gmail.com>
2025-07-16 22:58:26 +0100

    created the configuration for project and ran migrations

---
+++ TaskManagement.Persistence/Configurations/ProjectConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.Projects;
+using TaskManagement.Persistence.Utils;
+
+namespace TaskManagement.Persistence.Configurations;
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable(nameof(TaskManagementDbContext.Projects), schema: PersistenceConstants.TaskManagement_SCHEMA);
+
+        builder.Property(p => p.Title)
+               .IsRequired()
+               .HasMaxLength(200);
+
+        builder.Property(p => p.Description)
+               .HasMaxLength(1000);
+
+        builder.Property(p => p.StartDate)
+               .IsRequired();
+
+        builder.Property(p => p.EndDate)
+               .IsRequired();
+
+        builder.Property(p => p.ProjectStatus)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasDefaultValue(ProjectStatus.Not_Started);
+
+        builder.Property(p => p.CreatedById)
+               .IsRequired();
+
+        builder.HasOne(p => p.CreatedBy)
+               .WithMany()
+               .HasForeignKey(p => p.CreatedById)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasMany(p => p.Tasks)
+               .WithOne(t => t.Project)
+               .HasForeignKey(t => t.ProjectId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ TaskManagement.Persistence/Migrations/20250716215037_ProjectMigration.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagement.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProjectMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Projects",
+                newName: "Projects",
+                newSchema: "taskManagement_schema");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectStatus",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "Not_Started",
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Users_CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects",
+                column: "CreatedById",
+                principalSchema: "taskManagement_schema",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Users_CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                schema: "taskManagement_schema",
+                table: "Projects");
+
+            migrationBuilder.RenameTable(
+                name: "Projects",
+                schema: "taskManagement_schema",
+                newName: "Projects");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ProjectStatus",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "Not_Started");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true);
+        }
+    }
+}
