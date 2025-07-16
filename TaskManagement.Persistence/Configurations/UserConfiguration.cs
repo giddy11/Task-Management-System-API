916ec4c5072d919d1682256c3dae09443c6fbfbb
giddy11 <edoghotugiddy@gmail.com>
2025-07-16 10:52:43 +0100

    Add User entity configuration with schema and property constraints

---
+++ TaskManagement.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TaskManagement.Domain.UserManagement;
+using TaskManagement.Persistence.Utils;
+
+namespace TaskManagement.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable(nameof(TaskManagementDbContext.Users), schema: PersistenceConstants.TaskManagement_SCHEMA);
+
+        // Properties
+        builder.Property(u => u.Email)
+               .IsRequired()
+               .HasMaxLength(255);
+
+        builder.Property(u => u.FirstName)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.Property(u => u.LastName)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.Property(u => u.PasswordHash)
+               .HasMaxLength(500);
+
+        builder.Property(u => u.AccountType)
+               .IsRequired()
+               .HasConversion<string>();
+
+        builder.Property(u => u.UserStatus)
+               .IsRequired()
+               .HasConversion<string>()
+               .HasDefaultValue(UserStatus.Active);
+
+        builder.HasIndex(u => u.Email)
+               .IsUnique()
+               .HasDatabaseName("IX_Users_Email");
+    }
+}
