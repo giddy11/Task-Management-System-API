543c7c1cb40d90c368088017adae528585190fa2
giddy11 <edoghotugiddy@gmail.com>
2025-07-17 11:27:13 +0100

    run migration for comment

--- TaskManagement.Domain/Comment.cs
+++ TaskManagement.Domain/Comment.cs
@@ -7,8 +7,10 @@ public class Comment
 {
     public Guid Id { get; init; }
     public Guid UserId { get; set; }
-    public Guid TaskId { get; set; }
+    public Guid TodoTaskId { get; set; }
     public User User { get; set; } = default!;
-    public TodoTask Task { get; set; } = default!;
+    public TodoTask TodoTask { get; set; } = default!;
     public string Content { get; set; } = default!;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
 }
